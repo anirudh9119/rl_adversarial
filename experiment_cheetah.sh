9632ad18c28d7ab3da56114ac57de5d84d242d86
AndroidCloud <anirudhgoyal9119@gmail.com>
2018-01-15 14:38:23 -0500

    updates

--- experiment_cheetah.sh
+++ experiment_cheetah.sh
@@ -4,7 +4,11 @@ source ~/.bashrc
 #export THEANO_FLAGS=...
 export PYTHONUNBUFFERED=1
 echo Running on $HOSTNAME
-python fwbw_mf.py --seed=0 --save_trpo_run_num=1 --which_agent=4 --num_workers_trpo=32 --yaml_file='cheetah_forward' --fw_learning_rate 0.001 --bw_learning_rate 0.0005 --num_imagination_steps 50 --fw_iter 1 --top_k_trajectories 100 --policy_variance  0 --bw_model_hidden_size 64
+python fwbw_mf.py --seed=0 --save_trpo_run_num=1 --which_agent=4 --num_workers_trpo=32 --yaml_file='cheetah_forward' --fw_learning_rate 0.001 --bw_learning_rate 0.0001 --num_imagination_steps 10 --fw_iter 1 --top_k_trajectories 100 --policy_variance  0 --bw_model_hidden_size 64 --num_trpo_iters 2 --nEpoch 20  --top_k_trajectories_state_selection 1 --use_good_trajectories 50
+
+
+
+#python fwbw_mf.py --seed=0 --save_trpo_run_num=1 --which_agent=4 --num_workers_trpo=32 --yaml_file='cheetah_forward' --fw_learning_rate 0.001 --bw_learning_rate 0.0005 --num_imagination_steps 50 --fw_iter 1 --top_k_trajectories 100 --policy_variance  0 --bw_model_hidden_size 64
 
 
 #For running TRPO baseline run this
